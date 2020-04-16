5a2dfb6f35425c0ca033fd40f6d307c674460c56
Jacob Gold <jkobygold@gmail.com>
2020-04-16 16:56:37 -0400

    work in progress

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "rootroot",
-    database: "bamazon" 
+    database: "bamazon"
 });
 
 connection.connect(function (err) {
@@ -17,17 +17,39 @@ connection.connect(function (err) {
 });
 
 var Table = function () {
-    connection.query("SELECT * FROM stuff", function (err,res ) {
+    connection.query("SELECT * FROM stuff", function (err, res) {
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].item_id);
             console.log(res[i].product_name);
             console.log(res[i].department_name);
             console.log(res[i].price);
-            console.log(res[i].stock_quantity); 
+            console.log(res[i].stock_quantity);
         }
-          if(err){
+        if (err) {
             console.log("Something Didn't work here");
         }
+        promptBuyer(res);
     }
     );
+};
+
+var promptBuyer = function (res) {
+    inquirer.prompt([{
+        type: "input",
+        name: 'choice',
+        message: " Hi there! Welcome to Bamazon! Are you interested in any of the followings products? "
+    }]).then(function (answer) {
+        var IwantThis = false;
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].product_name === answer.choice) {
+                var IwantThis = true;
+                var item = answer.choice;
+                var id = i;
+         
+            }
+        }
+    });
+
+
+
 };
